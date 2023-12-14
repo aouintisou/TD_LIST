310473bd0e896057c2a988247644a5cb92756930
aouintisou <149176781+aouintisou@users.noreply.github.com>
2023-12-14 20:23:28 +0100

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+ 
+        //Ecrire un programme main permettant de tester toutes les méthodes de la classe.
+       
+        Etudiant e1 = new Etudiant("sallami", "baha", 1);
+        Etudiant e2 = new Etudiant("ahmed", "ahmed", 1);
+        Etudiant e3 = new Etudiant("neifer", "sami", 2);
+        
+        System.out.println(e1.equals(e2));
+        System.out.println(e1.equals(e3));
+
+        System.out.println(e1.toString());
+        System.out.println(e2.toString());
+        System.out.println(e3.toString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
